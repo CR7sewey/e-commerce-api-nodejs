fefbbb931ed8606d7a7c741997c9a14652856569
CR7sewey <miguelmotacarvalho@gmail.com>
2024-04-29 17:42:42 +0100

    Just testing the server

---
+++ README.md
@@ -0,0 +1,7 @@
+## Hosted Project
+
+[E-Commerce API Render URL]
+#### Setup Basic Express Server
+
+- [] import express and assign to variable
+- [] setup start port variable (5000) and start function
--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('E-Commerce API');
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send("E-commerce Project");
+});
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    // later db connection
+    app.listen(port, () => {
+      console.log("Im listening on port ", port);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
