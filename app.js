ec7f1386d485a878445564f43e32a4073b10a2b8
CR7sewey <miguelmotacarvalho@gmail.com>
2024-04-29 17:48:56 +0100

    Setting up some usual middlewares and errors

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const errorHandlerMiddleware = require("./middlewares/error-handler");
+const notFound = require("./middlewares/not-found");
+
 require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("E-commerce Project");
 });
+
+// Middlewares
+app.use(errorHandlerMiddleware);
+app.use(notFound);
 
 const port = process.env.PORT || 3000;
 const start = async () => {
