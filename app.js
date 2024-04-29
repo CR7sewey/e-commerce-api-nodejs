fde49c2edc6b205717086dbf71a0160084be0d86
CR7sewey <miguelmotacarvalho@gmail.com>
2024-04-29 18:09:14 +0100

    More middlewares

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ const app = express();
 const errorHandlerMiddleware = require("./middlewares/error-handler");
 const notFound = require("./middlewares/not-found");
 const dbConnection = require("./db/connect");
-
+require("express-async-errors");
 require("dotenv").config();
+
+// Middlewares
+app.use(express.json()); // access to json data in req.body
 
 app.get("/", (req, res) => {
   res.send("E-commerce Project");
 });
 
 // Middlewares
-app.use(errorHandlerMiddleware);
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 const start = async () => {
