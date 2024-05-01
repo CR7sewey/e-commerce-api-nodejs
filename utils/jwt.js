910dde447dbdae9fdf96f388669676ef8d086449
CR7sewey <miguelmotacarvalho@gmail.com>
2024-05-01 11:31:17 +0100

    Cookies setup - signed and secure flags - part 5

--- utils/jwt.js
+++ utils/jwt.js
@@ -15,6 +15,8 @@ const attachCookiesToResponse = ({ res, token }) => {
   res.cookie("token", token, {
     httpOnly: true,
     expires: new Date(Date.now() + 24 * 3600000), // 24 hours = 24 x 60 minutes x 60 seconds x 1000 (bcs in milis)
+    secure: process.env.NODE_ENV === "production", // ture or false
+    signed: true,
   });
 };
 
