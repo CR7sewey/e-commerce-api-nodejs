5487fb4cdfad5362c6bf60ef60146180f2e71fa1
CR7sewey <miguelmotacarvalho@gmail.com>
2024-05-06 17:49:10 +0100

    Review controller - delete and update

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,6 +3,7 @@ const Product = require("../models/Product");
 const BadRequest = require("../errors/bad-request");
 const { StatusCodes } = require("http-status-codes");
 const NotFound = require("../errors/not-found");
+const checkPermissions = require("../utils/checkPermissions");
 
 const createReview = async (req, res) => {
   // can do some vals but the review model already does it (namely missing value or repetion)
@@ -39,11 +40,36 @@ const getSingleReview = async (req, res) => {
 };
 
 const updateReview = async (req, res) => {
-  res.send("review 4");
+  const { id } = req.params;
+  /*const review = await Review.findByIdAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });*/
+  const review = await Review.findOne({ _id: id });
+
+  if (!review) {
+    throw new NotFound("This review doesnt exists.");
+  }
+  checkPermissions(req.user, review.user);
+
+  review.rating = req.body.rating || review.rating;
+  review.comment = req.body.comment || review.comment;
+  review.title = req.body.title || review.title;
+
+  await review.save();
+
+  return res.status(StatusCodes.OK).json({ review });
 };
 
 const deleteReview = async (req, res) => {
-  res.send("review 5");
+  const { id } = req.params;
+  const review = await Review.findOne({ _id: id });
+  if (!review) {
+    throw new NotFound("This review doesnt exists.");
+  }
+  checkPermissions(req.user, review.user);
+  await Review.deleteOne({ _id: id });
+  return res.status(StatusCodes.OK).json({ msg: "Success. Product removed!" });
 };
 
 module.exports = {
