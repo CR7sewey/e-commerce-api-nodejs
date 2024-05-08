1c441f1eebe2384a54760e9ec4005c9ae1101424
CR7sewey <miguelmotacarvalho@gmail.com>
2024-05-08 17:25:37 +0100

    Aggregate Setup - How to update automatically averageRatings - step 1

--- models/Product.js
+++ models/Product.js
@@ -58,6 +58,10 @@ const ProductSchema = mongoose.Schema(
       type: Number,
       default: 0,
     },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
     user: {
       type: mongoose.Types.ObjectId,
       ref: "User", // reference to User model
